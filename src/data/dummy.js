6d5b688793851fe54954382790217bb86c0a0dbd
busy-joj <busy.joj@gmail.com>
2024-01-04 18:51:08 +0900

    add dummy data

---
+++ src/data/dummy.js
@@ -0,0 +1,41 @@
+export const dummy = [
+  {
+    problemNum: "5427",
+    problemLink: "/problem/5427",
+    language: "Python 3",
+    solvedTime: "2023-08-06 18:03:27",
+  },
+  {
+    problemNum: "16397",
+    problemLink: "/problem/16397",
+    language: "PyPy3",
+    solvedTime: "2023-07-31 08:18:40",
+  },
+  {
+    problemNum: "16397",
+    problemLink: "/problem/16397",
+    language: "Python 3",
+    solvedTime: "2023-07-31 08:17:44",
+  },
+  {
+    problemNum: "2206",
+    problemLink: "/problem/2206",
+    language: "PyPy3",
+    solvedTime: "2023-07-31 05:40:49",
+  },
+  {
+    solvedTime: "2023-07-31 05:38:34",
+  },
+  {
+    problemNum: "5427",
+    problemLink: "/problem/5427",
+    language: "Python 3",
+    solvedTime: "2023-07-31 00:17:47",
+  },
+  {
+    problemNum: "9663",
+    problemLink: "/problem/9663",
+    language: "PyPy3",
+    solvedTime: "2023-07-30 09:54:12",
+  },
+];
