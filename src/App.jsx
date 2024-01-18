299b14b530627347e38f39f7646064601fd89cd0
jyj <busy.joj@gmail.com>
2024-01-18 09:11:23 +0900

    add user API

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import { Reset } from "styled-reset";
 import Header from "./components/Header";
 import Table from "./components/Table";
 import Contribution from "./components/Contribution";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProfilePage from "./pages/profile";
 
 const App = () => {
   return (
     <>
-      <Reset />
-      <Header />
-      <div className=" w-[1200px] my-0 mx-auto pt-5">
+      <BrowserRouter>
+        <Reset />
+        <Header />
+        <Routes>
+          <Route path="/profile" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <div className="w-[1200px] my-0 mx-auto pt-5">
         <Contribution />
         <Table />
-      </div>
+      </div> */}
     </>
   );
 };
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Contribution from "../../components/Contribution";
+import Table from "../../components/Table";
 
 const ProfilePage = () => {
+  const [fetchSolvedProblem, setFetchSolvedProblem] = useState();
   useEffect(() => {
     const fetchData = async () => {
       await axios
         .get("http://localhost:8080/pet")
         .then((res) => {
-          console.log(res.data);
+          setFetchSolvedProblem(res.data);
         })
         .catch((error) => {
           console.log("Error", error);
         });
     };
-    console.log(fetchData());
-    const weeks = groupDatesByWeeks(allActivities);
-    setDatesByWeeks(weeks);
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div>ProfilePage</div>;
+  return (
+    <div className="w-[1200px] my-0 mx-auto pt-5">
+      <Contribution fetchSolvedProblem={fetchSolvedProblem} />
+      <Table fetchSolvedProblem={fetchSolvedProblem} />
+    </div>
+  );
 };
 
 export default ProfilePage;
