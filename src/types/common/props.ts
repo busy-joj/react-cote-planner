650628d821a6750b74a799420e9fde0424100429
jyj <busy.joj@gmail.com>
2024-03-24 17:32:57 +0900

    fix console삭제, import React

--- src/types/common/props.ts
+++ src/types/common/props.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default interface IDefaultProps{
   className?: string,
   children?: React.ReactNode
--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -2,6 +2,5 @@ import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export const cn = (...inputs: string[]) => {
-  console.log(inputs)
   return twMerge(clsx(inputs));
 };
