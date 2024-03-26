f03e31cf6da14c872f045d9450bb6d3c1fc32b7a
taemin-Jang <jtm990506@gmail.com>
2024-03-26 14:53:11 +0900

    refactor Header, AvatarButton 컴포넌트 js to ts

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -1,11 +1,17 @@
-import { useEffect } from 'react';
+import { RefObject, useEffect } from 'react';
 
-export default function useOnClickOutside(ref, handler) {
+export default function useOnClickOutside(
+  ref: RefObject<HTMLDivElement>,
+  handler: () => void,
+) {
   useEffect(() => {
-    const listener = e => {
+    const listener = (e: MouseEvent | TouchEvent) => {
+      if (!(e.target instanceof Node)) return;
+
       if (!ref.current || ref.current.contains(e.target)) {
         return;
       }
+
       handler();
     };
     document.addEventListener('mousedown', listener);
