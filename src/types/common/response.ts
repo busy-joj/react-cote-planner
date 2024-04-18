bf09a86aff16616c8dbc8d4a14193c644d154c8f
SeungJai Hyun <dudtod1596@gmail.com>
2024-04-18 14:06:52 +0900

    Revert "Test setting jest rtl"

--- src/types/common/response.ts
+++ src/types/common/response.ts
@@ -1,11 +1,9 @@
 export interface ResponseData<T> {
-  data: T;
+  data: T
 }
 
-export interface ICommonResponse<T> extends ResponseData<T> {
+export interface ICommonResponse<T> extends ResponseData<T>{
   code: 'SUCCESS' | 'ERROR' | 'FAIL';
   message: string;
   statusCode: number;
 }
-
-export type ResponseCode = 200 | 404 | 403 | 401 | 402;
--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,34 @@
-/// <reference types="vitest" />
 import { defineConfig, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from "rollup-plugin-visualizer";
 
-export default ({ mode }: { mode: string }) => {
-  return defineConfig({
-    plugins: [
-      svgr({
-        svgrOptions: {
-          // svgr options
-        },
-      }),
-      react(),
-      mode === 'analyze'
-        ? (visualizer({
-            template: 'treemap', // or sunburst, treemap, network
-            open: true,
-            gzipSize: true,
-            brotliSize: true,
-            filename: 'analyze.html', // will be saved in project's root
-          }) as PluginOption)
-        : [],
-    ],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/test/setup.ts',
-    },
-    resolve: {
-      alias: [
-        { find: '@', replacement: path.resolve(__dirname, './src') },
-        {
-          find: '@components',
-          replacement: path.resolve(__dirname, './src/components'),
-        },
+export default ({mode}:{mode:string})=>{
+  return defineConfig ({
+      plugins: [
+        svgr({
+          svgrOptions: {
+            // svgr options
+          },
+        }),
+        react(),
+        mode === "analyze" ?
+        visualizer({
+          template: "treemap", // or sunburst, treemap, network
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+          filename: "analyze.html", // will be saved in project's root
+        }) as PluginOption : [],
       ],
+      resolve: {
+        alias: [
+          { find:'@', replacement:path.resolve(__dirname, './src')},
+          { find:"@components", replacement:path.resolve(__dirname, './src/components')}
+        ]
+      }
     },
-  });
-};
+  );
+
+}
